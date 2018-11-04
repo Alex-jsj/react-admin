3b0e3a4a07330d26596baa6973bffb682041baf2
alex <chenzeyongjsj@163.com>
2018-11-04 22:29:32 +0800

    项目结构初始化

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import { Button } from "antd";
+import React, { Component, Fragment } from "react";
+import { Row, Col } from "antd";
 import "./App.css";
 
-export default class App extends React.Component {
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Nav from "./components/nav";
+
+export default class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <Button>Click</Button>
-      </div>
+      <Fragment>
+        <Row>
+          <Col span="3">
+            <Nav />
+          </Col>
+          <Col span="21">
+            <Header />
+            <Row>Content</Row>
+            <Footer />
+          </Col>
+        </Row>
+      </Fragment>
     );
   }
 }
---
+++ src/components/nav/index.js
@@ -0,0 +1,7 @@
+import React, { Component, Fragment } from "react";
+
+export default class Nav extends Component {
+  render() {
+    return <Fragment>Nav</Fragment>;
+  }
+}
